f3f83cfc120969f966b85e74bb5d6bd608562450
yelvertoncory <31113087+yelvertoncory@users.noreply.github.com>
2018-05-06 14:38:01 -0400

    Add files via upload

---
+++ app/src/main/java/com/example/isaac/youbudget/DataHelper.java
@@ -0,0 +1,47 @@
+package com.example.isaac.youbudget;
+
+/**
+ * Created by Cory Yelverton on 5/6/2018.
+ */
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+
+public class DataHelper extends SQLiteOpenHelper{
+
+    private static final String DATABASE_NAME = "Budget.db";
+    private static final int DATABASE_VERSION = 1;
+    public DataHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        // TODO Auto-generated constructor stub
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+        String sql = "create table budget1(" +
+                "dt datetime null, " +
+                "title text null, " +
+                "total int null, " +
+                "Rent int null, " +
+                "Food int null" +
+                "Groceries int null" +
+                "Gas int null" +
+                "Entertainment int null" +
+                "Savings int null" +
+                "Utilities int null" +
+                "Insurance int null" +
+                "OtherExpenses int null)";
+        //Log.d("Journal", "onCreate: " + sql);
+        //db.execSQL(sql);
+        //String sql2 = "create table account(username text, password text, email text);";
+        //Log.d("Data Entry", "onCreate: " + sql2);
+        //db.execSQL(sql2);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2)
+    {
+        // TODO Auto-generated method stub
+    }
+}
